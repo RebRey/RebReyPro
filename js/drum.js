652a3c60745c5b185f1ed8b200d24bc692da165b
Rebecca Reyes <rebreyaz@gmail.com>
2022-07-05 13:42:21 -0700

    drum keyboard press response

--- js/drum.js
+++ js/drum.js
@@ -1,9 +1,35 @@
+// Detects Button Clicks
+
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
+
+    // call makeSound, passing in buttonInnerHTML 
+    makeSound(buttonInnerHTML);
+  });
+}
+
+// Detects keyboard presses
+// Add an event listener to the entire document, so that the entire web page 
+// starts listening for keyboard strokes
+// When that function gets triggered, add an option to pass in a parameter (event or e)/
+// This allows you to tap into the event that triggered the event listener.
+document.addEventListener("keydown", function(event){
+  
+  // Call the makeSound function and pass in event.key
+  // We can pass functions as parameters and have functions as callbacks which include
+  // things that we get back after the event has happened
+  makeSound(event.key)
+});
+
+// Create a new function called makeSound and its going to take a single parameter
+// which is going to be the key. 
+function makeSound(key){
+   // Use switch statement that checks to see which key was clicked on and pressed 
+    // responds by playing the relevant sound
+    switch (key) {
       case "w":
         var tom1 = new Audio("sounds/tom-1.mp3");
         tom1.play();
@@ -40,15 +66,4 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
         break;
       default: console.log(buttonInnerHTML);
     }
-  });
 }
-
-// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
-
-// for (var i = 0; i < numberOfDrumButtons; i++) {
-//   document.querySelectorAll(".drum")[i].addEventListener("click", play);
-//   function play() {
-//     var audio = new Audio("sounds/tom-1.mp3");
-//     audio.play();
-//   }
-// }
